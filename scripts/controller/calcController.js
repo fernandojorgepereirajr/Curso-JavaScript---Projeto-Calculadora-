75959c769f1b27ebbdc5685f47704c19b89be8a7
Fernando Jorge <fernando.junior@ccc.ufcg.edu.br>
2020-02-19 20:58:47 -0300

    Operações dos botões

---
+++ scripts/controller/calcController.js
@@ -0,0 +1,72 @@
+class CalcController{
+    constructor(){
+        this._locale = 'pt-BR'
+        this._displayCalcEl = document.querySelector('#display')
+        this._dateEl = document.querySelector('#data')
+        this._timeEl = document.querySelector('#hora')
+
+        this._currentDate
+        this.intialize()
+        this.initButtonEvents()
+
+    }
+
+    intialize(){
+        setInterval(()=>{
+            this.setDisplayDateTime()
+        }, 1000)
+
+
+    }
+    addEventListenerAll(element, events, fn) {
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false)
+        })
+
+    }
+    initButtonEvents(){
+        let buttons  = document.querySelectorAll('#buttons > g , #parts > g')
+        
+        buttons.forEach(btn=>{
+            this.addEventListenerAll( btn,'click drag', e =>{
+                console.log(btn.className.baseVal.replace('btn-', ''))
+            })
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e =>{
+                btn.style.cursor = 'pointer'
+            })
+        })
+    }
+
+    setDisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale)
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
+    }
+    get displayTime(){
+        this._timeEl.innerHTML
+    }
+    set displayTime(value){
+        this._timeEl.innerHTML = value
+    }
+
+    get displayDate(){
+        this._dateEl.innerHTML
+    }
+    set displayDate(value){
+        this._dateEl.innerHTML = value
+    }
+
+    get displayCalc(){
+        return this._displayCalcEl.innerHTML
+    }
+
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value
+    }
+
+    get currentDate(){
+        return new Date()
+    }
+    set currentDate(value){
+        this._currentDate =  value
+    }
+}
